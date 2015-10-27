d0cf96ef86579cbc6bf78eb0fff282fbc4df0016
a-isaac <amroisaac1@gmail.com>
2015-10-27 11:31:56 -0400

    abstractAccount
    Might need to turn this into interface, not sure yet. 2 classes will
    inherit from abstractAccount, chequing & savings.

---
+++ Ex5/src/coe528/lab5/abstractAccount.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe528.lab5;
+
+/**
+ *
+ * @author a2isaac
+ */
+public class abstractAccount {
+    //testing this for later
+}
