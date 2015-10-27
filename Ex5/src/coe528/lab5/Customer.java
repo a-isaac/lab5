f9cfd082962e890ff142c032ae098f1b0f8dce89
a-isaac <amroisaac1@gmail.com>
2015-10-27 11:27:03 -0400

    Upload Project

---
+++ Ex5/src/coe528/lab5/Customer.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe528.lab5;
+
+/**
+ *
+ * @author a2isaac
+ */
+public class Customer {
+    private String name;
+    private String password;
+
+    public Customer(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+}
